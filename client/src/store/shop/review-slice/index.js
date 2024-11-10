27cae49320f2131278325cff529e0af26db4b67c
iamkkaushik <160194586+iamkkaushik@users.noreply.github.com>
2024-11-10 12:50:48 +0530

    Update12 index.js

--- client/src/store/shop/review-slice/index.js
+++ client/src/store/shop/review-slice/index.js
@@ -10,7 +10,7 @@ export const addReview = createAsyncThunk(
   "/order/addReview",
   async (formdata) => {
     const response = await axios.post(
-      `https://bh-ten.vercel.app/api/shop/review/add`,
+      `https://bh-server-ib7d.onrender.com/api/shop/review/add`,
       formdata
     );
 
@@ -20,7 +20,7 @@ export const addReview = createAsyncThunk(
 
 export const getReviews = createAsyncThunk("/order/getReviews", async (id) => {
   const response = await axios.get(
-    `https://bh-ten.vercel.app/api/shop/review/${id}`
+    `https://bh-server-ib7d.onrender.com/api/shop/review/${id}`
   );
 
   return response.data;
