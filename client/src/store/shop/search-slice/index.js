9067204ed8d1082253b13a7c240f5f73c0436123
iamkkaushik <160194586+iamkkaushik@users.noreply.github.com>
2024-11-10 12:51:15 +0530

    Update14 index.js

--- client/src/store/shop/search-slice/index.js
+++ client/src/store/shop/search-slice/index.js
@@ -10,7 +10,7 @@ export const getSearchResults = createAsyncThunk(
   "/order/getSearchResults",
   async (keyword) => {
     const response = await axios.get(
-      `https://bh-ten.vercel.app/api/shop/search/${keyword}`
+      `https://bh-server-ib7d.onrender.com/api/shop/search/${keyword}`
     );
 
     return response.data;
