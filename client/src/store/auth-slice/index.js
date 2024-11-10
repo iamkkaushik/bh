9f2d5a431fa50b959a402d911b51a18b842cb259
iamkkaushik <160194586+iamkkaushik@users.noreply.github.com>
2024-11-10 12:46:57 +0530

    Update5 index.js

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -12,7 +12,7 @@ export const registerUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "https://bh-ten.vercel.app/api/auth/register",
+      "https://bh-server-ib7d.onrender.com/api/auth/register",
       formData,
       {
         withCredentials: true,
@@ -28,7 +28,7 @@ export const loginUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "https://bh-ten.vercel.app/api/auth/login",
+      "https://bh-server-ib7d.onrender.com/api/auth/login",
       formData,
       {
         withCredentials: true,
@@ -44,7 +44,7 @@ export const logoutUser = createAsyncThunk(
 
   async () => {
     const response = await axios.post(
-      "https://bh-ten.vercel.app/api/auth/logout",
+      "https://bh-server-ib7d.onrender.com/api/auth/logout",
       {},
       {
         withCredentials: true,
@@ -60,7 +60,7 @@ export const checkAuth = createAsyncThunk(
 
   async () => {
     const response = await axios.get(
-      "https://bh-ten.vercel.app/api/auth/check-auth",
+      "https://bh-server-ib7d.onrender.com/api/auth/check-auth",
       {
         withCredentials: true,
         headers: {
