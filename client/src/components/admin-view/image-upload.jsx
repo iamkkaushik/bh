8973ac17cbd07e8cd77bdd86097e31b62bf536be
iamkkaushik <160194586+iamkkaushik@users.noreply.github.com>
2024-11-10 12:55:24 +0530

    Update16 image-upload.jsx

--- client/src/components/admin-view/image-upload.jsx
+++ client/src/components/admin-view/image-upload.jsx
@@ -50,7 +50,7 @@ function ProductImageUpload({
     const data = new FormData();
     data.append("my_file", imageFile);
     const response = await axios.post(
-      "https://bh-ten.vercel.app/api/admin/products/upload-image",
+      "https://bh-server-ib7d.onrender.com/api/admin/products/upload-image",
       data
     );
     console.log(response, "response");
