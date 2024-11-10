4e353161df1a69bc644bb49881ec7a92be1071ee
iamkkaushik <160194586+iamkkaushik@users.noreply.github.com>
2024-11-10 12:50:11 +0530

    Update10 index.js

--- client/src/store/shop/products-slice/index.js
+++ client/src/store/shop/products-slice/index.js
@@ -18,7 +18,7 @@ export const fetchAllFilteredProducts = createAsyncThunk(
     });
 
     const result = await axios.get(
-      `https://bh-ten.vercel.app/api/shop/products/get?${query}`
+      `https://bh-server-ib7d.onrender.com/api/shop/products/get?${query}`
     );
 
     console.log(result);
@@ -31,7 +31,7 @@ export const fetchProductDetails = createAsyncThunk(
   "/products/fetchProductDetails",
   async (id) => {
     const result = await axios.get(
-      `https://bh-ten.vercel.app/api/shop/products/get/${id}`
+      `https://bh-server-ib7d.onrender.com/api/shop/products/get/${id}`
     );
 
     return result?.data;
