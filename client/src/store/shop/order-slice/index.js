27ad436837beafa6be9f5187cf7a50b4e77af3a5
iamkkaushik <160194586+iamkkaushik@users.noreply.github.com>
2024-11-10 12:49:40 +0530

    Update9 index.js

--- client/src/store/shop/order-slice/index.js
+++ client/src/store/shop/order-slice/index.js
@@ -13,7 +13,7 @@ export const createNewOrder = createAsyncThunk(
   "/order/createNewOrder",
   async (orderData) => {
     const response = await axios.post(
-      "https://bh-ten.vercel.app/api/shop/order/create",
+      "https://bh-server-ib7d.onrender.com/api/shop/order/create",
       orderData
     );
 
@@ -25,7 +25,7 @@ export const capturePayment = createAsyncThunk(
   "/order/capturePayment",
   async ({ paymentId, payerId, orderId }) => {
     const response = await axios.post(
-      "https://bh-ten.vercel.app/api/shop/order/capture",
+      "https://bh-server-ib7d.onrender.com/api/shop/order/capture",
       {
         paymentId,
         payerId,
@@ -41,7 +41,7 @@ export const getAllOrdersByUserId = createAsyncThunk(
   "/order/getAllOrdersByUserId",
   async (userId) => {
     const response = await axios.get(
-      `https://bh-ten.vercel.app/api/shop/order/list/${userId}`
+      `https://bh-server-ib7d.onrender.com/api/shop/order/list/${userId}`
     );
 
     return response.data;
@@ -52,7 +52,7 @@ export const getOrderDetails = createAsyncThunk(
   "/order/getOrderDetails",
   async (id) => {
     const response = await axios.get(
-      `https://bh-ten.vercel.app/api/shop/order/details/${id}`
+      `https://bh-server-ib7d.onrender.com/api/shop/order/details/${id}`
     );
 
     return response.data;
